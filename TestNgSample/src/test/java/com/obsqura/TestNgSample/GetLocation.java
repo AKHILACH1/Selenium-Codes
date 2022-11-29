17f717c77dc3bba0e0fc06b2be5dc623d70695a2
AKHILACH1 <kathucek@gmail.com>
2022-11-29 20:00:16 +0530

    updated version

--- TestNgSample/src/test/java/com/obsqura/TestNgSample/GetLocation.java
+++ TestNgSample/src/test/java/com/obsqura/TestNgSample/GetLocation.java
@@ -1,5 +1,7 @@
 package com.obsqura.TestNgSample;
 
+import static org.testng.Assert.assertEquals;
+
 import java.util.List;
 
 import org.openqa.selenium.By;
@@ -73,26 +75,59 @@ public class GetLocation extends Base {
 	
 	@Test
 	public void checkRadioButtonMale() {
-		WebElement radioButtonMale,buttonselect;
+		WebElement radioButtonMale,buttonselect,showMsg;
 		boolean isSelected;
+		String inputMessage="Radio button 'Male' is checked",expectedMsg;
+		driver.navigate().to("https://selenium.obsqurazone.com/radio-button-demo.php");
 		radioButtonMale= driver.findElement(By.xpath("//input[@id='inlineRadio1']"));
-		buttonselect=driver.findElement(By.xpath("//button[@='button-one']"));
+		buttonselect=driver.findElement(By.xpath("//button[@id='button-one']"));
+		showMsg=driver.findElement(By.xpath("//div[@id='message-one']"));
 		radioButtonMale.click();
 		isSelected=radioButtonMale.isSelected();
 		if(isSelected) {
 			buttonselect.click();
+			expectedMsg=showMsg.getText();
+			Assert.assertEquals(expectedMsg, inputMessage);
 			
 		}
-		
-	
+	}
+	@Test
+	public void checkRadioButtonFemale() {
+		WebElement radioButtonFemale,buttonselect,showMsg;
+		boolean isSelected;
+		String inputMessage="Radio button 'Female' is checked",expectedMsg;
+		driver.navigate().to("https://selenium.obsqurazone.com/radio-button-demo.php");
+		radioButtonFemale= driver.findElement(By.xpath("//input[@id='inlineRadio2']"));
+		buttonselect=driver.findElement(By.xpath("//button[@id='button-one']"));
+		showMsg=driver.findElement(By.xpath("//div[@id='message-one']"));
+		radioButtonFemale.click();
+		isSelected=radioButtonFemale.isSelected();
+		if(isSelected) {
+			buttonselect.click();
+			expectedMsg=showMsg.getText();
+			Assert.assertEquals(expectedMsg, inputMessage);
+			
+		}
+	}
+	@Test
+	public void checkRadioButtonNone() {
+		WebElement radioButtonFemale,radioButtonMale,buttonselect,showMsg;
+		boolean isMaleSelected,isFemaleSelected;
+		String inputMessage="Radio button is Not checked",expectedMsg;
+		driver.navigate().to("https://selenium.obsqurazone.com/radio-button-demo.php");
+		radioButtonMale= driver.findElement(By.xpath("//input[@id='inlineRadio1']"));
+		radioButtonFemale= driver.findElement(By.xpath("//input[@id='inlineRadio2']"));
+		buttonselect=driver.findElement(By.xpath("//button[@id='button-one']"));
+		showMsg=driver.findElement(By.xpath("//div[@id='message-one']"));
+		isMaleSelected=radioButtonMale.isSelected();
+		isFemaleSelected=radioButtonFemale.isSelected();
+		if((isMaleSelected==false)&&(isFemaleSelected==false)) {
+			buttonselect.click();
+			expectedMsg=showMsg.getText();
+			Assert.assertEquals(expectedMsg, inputMessage);
+			
+		}
 	}
 }
 
-/*
- * Verify Show message button place under enter message field.
- * (https://selenium.obsqurazone.com/simple-form-demo.php)
- * 
- * Click on date picker category from menu option using find elements
- * (https://selenium.obsqurazone.com/)
- * 
- */
+
